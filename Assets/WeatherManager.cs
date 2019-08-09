9c033a84328e7cac1ec3f823eecb6335c1e8438e
Pawan Kumar Dubey <cooltoadkumar18@gmail.com>
2019-08-09 09:43:42 +0530

    Initial commit

---
+++ Assets/WeatherManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+
+    public GameObject rain;
+    public GameObject evaporation;
+    public GameObject condensation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rain.SetActive(false);
+        evaporation.SetActive(false);
+        condensation.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            rain.SetActive(false);
+            condensation.SetActive(false);
+            evaporation.SetActive(true);
+        }
+        if(Input.GetKey(KeyCode.C))
+        {
+            evaporation.SetActive(true);
+            condensation.SetActive(true);
+            rain.SetActive(false);
+        }
+        if(Input.GetKey(KeyCode.R))
+        {
+            rain.SetActive(true);
+            condensation.SetActive(true);
+            evaporation.SetActive(false);
+        }
+        
+    }
+    public void Rain()
+    {
+        rain.SetActive(true);
+        condensation.SetActive(true);
+        evaporation.SetActive(false);
+    }
+    
+    
+    public void Evaporation()
+    {
+        rain.SetActive(false);
+        condensation.SetActive(false);
+        evaporation.SetActive(true);
+    }
+    public void Condensation()
+    {
+        evaporation.SetActive(true);
+        condensation.SetActive(true);
+        rain.SetActive(false);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# WaterCycle 3D Simulation
+ This is a water cycle 3D scene to understand the process of water cycle visually.
